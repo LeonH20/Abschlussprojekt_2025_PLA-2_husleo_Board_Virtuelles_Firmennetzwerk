c1578ebc2c5c437f3b1d668514e2d2e2831d9ede
LeonH20 <leon.huskaj1212@gmail.com>
2025-06-27 09:31:45 +0200

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../style.css">
+<div class="nav">
+  <a href="../home.php">Home</a>
+  <a href="../uploads.php">Uploads</a>
+  <a href="../docs.php">Dokumentationen</a>
+  <a href="../news.php">News</a>
+  <a href="../logout.php">Logout</a>
+</div>
+<hr>
