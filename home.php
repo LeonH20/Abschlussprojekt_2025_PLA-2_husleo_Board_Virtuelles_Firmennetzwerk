eea78363664038e3ceca099195285b04ce17642c
LeonH20 <leon.huskaj1212@gmail.com>
2025-06-27 09:27:11 +0200

    Create home.php

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+include 'includes/auth.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include 'includes/header.php'; ?>
+<h1>Willkommen im Intranet</h1>
+<p>Bitte benutze die Navigation, um auf Inhalte zuzugreifen.</p>
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
