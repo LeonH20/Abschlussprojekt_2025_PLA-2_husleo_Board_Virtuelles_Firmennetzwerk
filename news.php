0061ba89ac42c5aede9ada592a69a67ee121bb75
LeonH20 <leon.huskaj1212@gmail.com>
2025-06-27 09:28:52 +0200

    Create news.php

---
+++ news.php
@@ -0,0 +1,16 @@
+<?php
+include 'includes/auth.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>News</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include 'includes/header.php'; ?>
+<h1>Aktuelle News</h1>
+<p>Bitte benutze die Navigation, um Neuigkeiten zu sehen., um auf Inhalte zuzugreifen.</p>
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
