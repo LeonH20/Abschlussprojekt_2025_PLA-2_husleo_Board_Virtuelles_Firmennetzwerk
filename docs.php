901854c6365e21ebd3c511c3aa10dcf9dff96c8e
LeonH20 <leon.huskaj1212@gmail.com>
2025-06-27 09:26:34 +0200

    Create docs.php

---
+++ docs.php
@@ -0,0 +1,16 @@
+<?php
+include 'includes/auth.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dokumentationen</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include 'includes/header.php'; ?>
+<h1>Dokumentationen</h1>
+<p>Bitte benutze die Navigation, um Dokumente anzusehen., um auf Inhalte zuzugreifen.</p>
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
